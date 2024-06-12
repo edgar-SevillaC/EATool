d07cad251ed613b4fbbb5c53d6d63a5106c1612f
Edgar Sevilla <hacker.sevilla@gmail.com>
2024-06-12 11:46:05 +0200

    Bug Fixes and improvement
    Bug fixes and improvements in traceability and dump features
    Include of Batch script for external Requirements tool

---
+++ EA Tool/ExtTool_SRS_export2excel_Template.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem This script shall extract SRS Data, with following columns (empty data is allowed). Extra columns can be added
+rem + SRS Document ID
+rem + Requirement Status
+rem + Requirement ID
+rem + Requirement description
+rem + Requirement Category
+
+
+
+
+set fileOutput="C:\Users\edgasevi\Documents\Internal Projects\EA Tool\specification_[ProjectName]_SRS_all.xls"
+
+echo SRS ExtTool command 
+
